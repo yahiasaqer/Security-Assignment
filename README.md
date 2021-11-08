cea134d03238a2746fee5f2f0b35bfdfb0524c42
yahiasaqer <yahiasaqer@gmail.com>
2021-11-08 23:33:41 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,34 @@
+# 🔎 Tracker - The Web Beacon API Demo
+
+The `Beacon API` schedules an asynchronous and non-blocking request to a web server. There are a few specialties of the beacon request,
+
+- Beacon requests typically do not require a response. This is a huge difference from the regular XHR or fetch requests where the client(browser) expects a response from the server.
+- Beacon requests are guaranteed to be initiated before a page is unloaded, even when you close the browser.
+- Beacon requests run to completion without requiring a blocking request (for example XMLHttpRequest).
+- Beacon requests use the HTTP POST method.
+
+This demo logs the time a user spent on the app with a simple message.
+
+# 🚀 How to run?
+Make sure you have `node.js` installed.
+
+- Clone the repository.
+- Change directory to the cloned project.
+- Do `npm install` or `yarn`.
+- Open a terminal(or command prompt) and, run the command `node app.js`.
+- Access the URL `http://localhost:3000/demo/`
+
+# Read more about it
+
+[https://blog.greenroots.info/how-to-log-user-activities-using-the-beacon-web-api-ckgq6s7k0094do9s15udf767u](https://blog.greenroots.info/how-to-log-user-activities-using-the-beacon-web-api-ckgq6s7k0094do9s15udf767u)
+
+# ⭐ Show your support
+Show your support with a star(⭐) if you liked the project. You are welcome to follow 🤝 me here to stay connected.
+
+## Many Thanks to all the `Stargazers` who has supported this project with stars(⭐)
+
+[![Stargazers repo roster for @atapas/tracker-beacon-api](https://reporoster.com/stars/atapas/tracker-beacon-api)](https://github.com/atapas/tracker-beacon-api/stargazers)
+
+
+
+Know about [my other projects from here](https://github.com/atapas#-my-show-off-projects).
---
+++ app.js
@@ -0,0 +1,95 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+var multer = require('multer');
+var url = require('url');
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./db/myDB.db');
+
+/*let sql = `SELECT * FROM victims`;
+
+db.all(sql, [], (err, rows) => {
+  if (err) {
+    throw err;
+  }
+  rows.forEach((row) => {
+    console.log(row.time + " " + row.url);
+  });
+});*/
+
+
+
+
+const forms = multer();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use('/demo', express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(forms.array()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(request, response){
+
+  db.all("SELECT * FROM victims", function(err,rows,fields) {
+  
+      console.log(rows);
+      console.log('This function will return a list of all stored items from database ' + rows);
+      response.setHeader('Content-Type','application/json')
+      response.send(JSON.stringify(rows));
+  
+  
+  });
+  });
+
+app.post('/log-tracking', function(req, res) {
+    console.log('**** Tracked. Now logging ****');
+    let body = req.body;
+
+    let startTime = body.start;
+    let endTime = body.end;
+    let trackInfo = body.msg;
+    var requrl = url.format({
+      protocol: req.protocol,
+      host: req.get('host'),
+      pathname: req.originalUrl,
+  });
+
+
+    let logMsg = '';
+
+    let time = (endTime - startTime) / 1000;
+    logMsg = `${time.toFixed(2)} seconds`;
+
+    if (time > 60) {
+        time = time / 60;
+        logMsg = `${time.toFixed(2)} minutes`;
+    }
+
+    console.log('In Session for: ', logMsg);
+    console.log('Tracking info: ', trackInfo);
+    require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+      console.log('ip address: ' + add); //getting the ip address
+    })
+    console.log("URL: "+ requrl); //getting the url
+
+  db.run(`INSERT INTO victims(time, url) VALUES(?,?)`, [logMsg.toString(), requrl.toString()], function(err) {
+  if (err) {
+    return console.log(err.message);
+  }
+  console.log(`A row has been inserted with rowid ${this.lastID}`);
+});
+
+
+    
+    
+});
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`User Tracking app listening at http://localhost:${port}`)
+})
